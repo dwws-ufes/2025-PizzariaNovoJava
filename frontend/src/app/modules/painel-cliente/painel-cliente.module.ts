1d3be01c00130f70434656ea41c82af0279a3290
layla.couto <laylamscouto@gmail.com>
2025-07-27 20:13:02 -0300

    Iniciando painel do garcom

--- frontend/src/app/modules/painel-cliente/painel-cliente.module.ts
+++ frontend/src/app/modules/painel-cliente/painel-cliente.module.ts
@@ -1,26 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { PainelClienteRoutingModule } from './painel-cliente-routing.module';
-import { PainelClienteComponent } from './painel-cliente/painel-cliente.component';
-import { PizzaPainelComponent } from './pizza-painel/pizza-painel.component';
-import { BebidaPainelComponent } from './bebida-painel/bebida-painel.component';
-import { SobremesaPainelComponent } from './sobremesa-painel/sobremesa-painel.component';
+import {PainelClienteRoutingModule} from './painel-cliente-routing.module';
+import {PainelGarcomComponent} from './painel-garcom/painel-garcom.component';
+import {PizzaPainelComponent} from './pizza-painel/pizza-painel.component';
+import {BebidaPainelComponent} from './bebida-painel/bebida-painel.component';
+import {SobremesaPainelComponent} from './sobremesa-painel/sobremesa-painel.component';
+import {SharedModule} from "../../shared/shared.module";
 
 
 @NgModule({
-    declarations: [
-        PainelClienteComponent,
-        PizzaPainelComponent,
-        BebidaPainelComponent,
-        SobremesaPainelComponent
-    ],
-    exports: [
-        PainelClienteComponent
-    ],
-    imports: [
-        CommonModule,
-        PainelClienteRoutingModule
-    ]
+  declarations: [
+    PainelGarcomComponent,
+    PizzaPainelComponent,
+    BebidaPainelComponent,
+    SobremesaPainelComponent
+  ],
+  exports: [
+    PainelGarcomComponent
+  ],
+  imports: [
+    CommonModule,
+    PainelClienteRoutingModule,
+    SharedModule
+  ]
 })
-export class PainelClienteModule { }
+export class PainelClienteModule {
+}
